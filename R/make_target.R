e1f807d4887206797551a76792520b3b56067c05
silvestrin.m <mateus_silvestrin@hotmail.com>
2021-11-18 15:18:39 -0300

    Add bifactor option to esem.

--- R/make_target.R
+++ R/make_target.R
@@ -3,8 +3,13 @@ make_target <- function (nitems, mainloadings){
   psych::scrub(target_mat, isvalue = 1)
 }
 
-esem <- function(data,  nfactors, target, fm = "ml", rotate = "TargetQ",...){
-  psych::fa(data, nfactors, fm = fm,
-            rotate = rotate, Target = target,...)
+esem <- function(data,  nfactors, target, bifactor = FALSE, fm = "pa", rotate = "targetQ",...){
+  ifelse(bifactor
+    ,esem_fit <- psych::fa(data, nfactors, fm = fm,
+                          rotate = "TargetT", Target = target,...)
+    ,esem_fit <- psych::fa(data, nfactors, fm = fm,
+                           rotate = rotate, Target = target,...)
+  )
+  esem_fit
 }
 # Tucker
