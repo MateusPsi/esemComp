0833a1c045beccfc6fea5f523c32f287295e2f36
silvestrin.m <mateus_silvestrin@hotmail.com>
2021-11-24 16:13:24 -0300

    Updated documentation. Added vignette. Added authors. Version 0.1!

--- R/syntax_composer.R
+++ R/syntax_composer.R
@@ -1,4 +1,11 @@
-# Given a psych::efa object, make a loadings dt for syntax_composer
+
+#' Make loadings DT
+#' Given a psych::efa object, make a loadings dt for syntax_composer
+#' @param fa_object
+#' @param factor_names
+#'
+#' @return long_loadings data.table
+#'
 make_loadings_dt <- function(fa_object, factor_names){
   nrow <- nrow(fa_object$loadings)
   nfactors <- length(factor_names)
@@ -20,6 +27,44 @@ make_loadings_dt <- function(fa_object, factor_names){
   long_loadings[]
 }
 
+#' ESEM-within-CFA syntax composer for lavaan
+#'
+#' @param efa_object A \cite{psych::fa()} object with the results of a exploratory factor analysis.
+#' The factor loadings from this analysis will be set as starting values or fixed values in the
+#' lavaan syntax created.
+#' @param referents A named character vector in the form `c(factor1 = "referent1", etc)`. Each
+#' entry identifies the referent for one of the factors. The order of this vector must be
+#' the same order the factors are ordered in the `efa_object`.
+#'
+#' @details ESEM-within-CFA models have latent variables estimations with all cross-loadings.
+#' To improve fit, starting values from a previous exploratory analysis are used. To ensure
+#' model identification, a referent is chosen for each latent variable and all their
+#' cross-loadings are set as fixed values. This function "composes" such a lavaan model
+#' syntax from an EFA object. This model can subsequently be used in lavaan with
+#' \cite{lavaan::cfa()} or \cite{lavaan::sem()}. The best way to check the output is
+#' with \cite{writeLines()}.
+#'
+#' It is very important to make sure the order of the `referents` vector is the same as the
+#' factor order in the factor loadings matrix in `efa_object`. Otherwise, the names of the
+#' latent variables may be wrong in the syntax. See also the vignette \cite{esem-within-cfa}.
+#'
+#' @return A character vector with a lavaan syntax for the ESEM model.
+#' @export
+#' @seealso \cite{make_target}
+#' @examples
+#' # use Holzinger and Swineford (1939) dataset in lavaan package
+#' hw_data <- lavaan::HolzingerSwineford1939
+#' hw_data <- hw_data[,c(7:15)]
+#'
+#' #make exploratory analysis with geomin rotation
+#' geomin_efa <- esem_efa(hw_data,3)
+#' referents_vector <- c(textual = "x5", visual = "x3", speed = "x7")
+#' model_syntax <- syntax_composer(geomin_efa, referents_vector)
+#' writeLines(model_syntax)
+#'
+#'# esem-within-cfa
+#'esem_w_cfa <- lavaan::cfa(model_syntax, data = hw_data)
+#'summary(esem_w_cfa)
 syntax_composer <- function(efa_object, referents){
 
   loadings_dt <- make_loadings_dt(efa_object, names(referents))
@@ -37,6 +82,6 @@ syntax_composer <- function(efa_object, referents){
   }
 
   # Put all syntaxes together
-  paste(sapply(unique(loadings_dt$latent), each_syntax), collapse = " ")
+  paste(sapply(unique(loadings_dt$latent), each_syntax), collapse = "\n")
 
 }
