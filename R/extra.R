aa8cfb232c84e28dc58caad40ec35c02d62b1299
silvestrin.m <mateus_silvestrin@hotmail.com>
2022-01-03 17:00:28 -0300

    Correct "cfa_fit" bug in omega_esem.

--- R/extra.R
+++ R/extra.R
@@ -9,10 +9,10 @@
 #' @examples
 omega_esem <- function(esem_model_fit, target_matrix){
   target_order <- dimnames(target_rot)[[2]]
-  loadings <- lavaan::lavInspect(cfa_fit,"std")[["lambda"]][,target_order]
+  loadings <- lavaan::lavInspect(esem_model_fit,"std")[["lambda"]][,target_order]
   loadings_sqr <- sum(abs(loadings[is.na(target_rot)]))^2
 
-  res_var <- lavaan::lavInspect(cfa_fit,"std")[["theta"]]
+  res_var <- lavaan::lavInspect(esem_model_fit,"std")[["theta"]]
   res_var <- sum(diag(res_var))
 
   loadings_sqr*(1/(loadings_sqr+res_var))
