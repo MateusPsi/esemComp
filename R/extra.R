3502c0cd23be7341051e2521f527e92d22dcfc3b
silvestrin.m <mateus_silvestrin@hotmail.com>
2022-01-07 15:45:33 -0300

    Updated Description. Updated omega_esem documentation. Added export_lavaan_summary documentation and "..." mods.

--- R/extra.R
+++ R/extra.R
@@ -40,16 +40,56 @@ omega_esem <- function(esem_model_fit, target_matrix){
 }
 
 
-export_lavaan_summary <- function(lavaan_model_fit, path_name = "lavaan_summary.txt", preamble = NULL){
+#' Export (dump) lavaan results
+#'
+#' @param lavaan_model_fit Lavaan object. Lavaan object with fitted ESEM model, usually obtained with `lavaan::cfa()`.
+#' @param path_name Character vector. Path directing where to save the text file with the results. It must end with the
+#' desired file name with the desired text file extension. The default is to save the file in the current directory
+#' with file name "lavaan_summary.txt".
+#' @param preamble Character vector. Allows the user to add some text before the results report. Useful to
+#' identify the analysis, add information about the data etc.
+#' @param ... `parameter = value` pairs. Additional modifiers to lavaan::summary() call. See the full list
+#' of available modifiers at \cite(lavaan-class).
+#'
+#' @details Runs `lavaan::summary()` on `lavaan_model_fit` with modifiers `std = TRUE`
+#' and `fit.measures = TRUE` and dumps the result to a text file. The user has the option to add some
+#' text as a preamble before the results description. It is also possible to add more information to the
+#' exported results by including more modifiers to the `summary` call with the `...` named values
+#' (e.g. `rsquare = TRUE` to include r-squared measures), see \cite(lavaan-class) for the full list
+#' of available modifiers.
+#'
+#' The `path_name` directories must be separated by "\" (even in Windows).
+#'
+#' @return Message confirming data dump. Saves the data to a text file as a side-effect.
+#' @export
+#'
+#' @examples
+#' HS.model <- ' visual  =~ x1 + x2 + x3
+#' textual =~ x4 + x5 + x6
+#' speed   =~ x7 + x8 + x9 '
+#' fit <- lavaan::cfa(HS.model, data= lavaan::HolzingerSwineford1939)
+#'
+#' export_lavaan_results(fit)
+#' # name the analysis with preamble
+#' export_lavaan_results(fit,
+#' path_name = "titled_lavaan_summary.txt",
+#' preamble = "CLASSIC HOLZINGER SWINEFORD CFA MODEL")
+#' # add r-squared measure to the exported results
+#' export_lavaan_results(fit,
+#' path = "lavaan_summary_rsquared.txt",
+#' rsquare = TRUE)
+
+export_lavaan_results <- function(lavaan_model_fit, path_name = "lavaan_summary.txt", preamble = NULL, ...){
   # check for file extension in path_name
   if(!grepl("\\.[[:alpha:]]{3}$", path_name)){
-    error_msg <- "It seems the path_name supplied does not end in an file extension.
+    error_msg <- "It seems the path_name supplied does not end in a file extension.
 Perhaps you forgot to finish with '.txt'?"
     rlang::abort(error_msg)
   }
 
   # save summary with preamble
-  summary_data <- capture.output(lavaan::summary(lavaan_model_fit, fit, fit.measures = T, std = T))
+  summary_data <- capture.output(
+    lavaan::summary(lavaan_model_fit, fit.measures = T, std = T, ...))
   cat(preamble,
       summary_data,
       file = path_name,
